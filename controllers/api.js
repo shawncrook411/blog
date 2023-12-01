c05f9421ea26f66a096087191a4b2dc62a915452
Shawn Crook <shawncrook411@gmail.com>
2023-11-30 18:27:19 -0700

    started adding partial handlebars

--- controllers/api.js
+++ controllers/api.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { BlogPost, User } = require('../models/index')
 
 router.get('/', async (req, res) => {
     try {
@@ -12,5 +13,29 @@ router.get('/login', async (req, res) => {
     } catch(err) {}
 })
 
+router.get('/dashboard', async (req, res) => {
+    try{
+        const blogs = await BlogPost.findAll({
+            include: [{ model: User, attributes: []}]
+        })
+        
+        console.log(blogs)
+        
+        
+        
+        
+        
+        
+        
+        res.render('dashboard', {
+            blogs,
+            // loggedIn: req.session.loggedIn
+        })
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
